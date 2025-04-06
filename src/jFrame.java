8b366ae007f8a19a7cc34919917a59f425318046
Zeosand <zeosandl@nycstudents.net>
2025-04-06 13:34:36 -0400

    JUMP ANIMATIONS RAAAHHHHHHHHHHHH

--- src/jFrame.java
+++ src/jFrame.java
@@ -1,32 +1,16 @@
 import javax.swing.JFrame;
 
-
 public class jFrame  {
 
     public jFrame() {
-
-        // create a JFrame (a window) and with a particular text for the title bar
         JFrame frame = new JFrame("Flappy Bird");
-        frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
-
-
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(750, 600);
-
         frame.setLocationRelativeTo(null);
 
-        frame.setVisible(true);
-
         DisplayPanel panel = new DisplayPanel();
-
         frame.add(panel);
-
         frame.setVisible(true);
-//        frame.addKeyListener(new DisplayPanel());
-
+        panel.requestFocusInWindow();
     }
-
-
-
-
-
 }
